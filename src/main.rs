e3fc4c37a9f8dacf669a20f6a61553d3709e8bd1
Christian Smith <smith.christian.william@gmail.com>
2024-05-15 13:01:19 -0400

    linprog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use linprog::{
+    Model,
+    Objective,
+    Summand,
+    Operator,
+    Var
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut model = Model::new("My LP", Objective::Max);
+    let mut vars: Vec<Var> = vec![];
+    vars.push(model.reg_var(2.0));
+    model.reg_constr(vec![Summand(1.0, &vars[0])], Operator::Le, 10.0);
+    model.optimize();
+    println!("{}", model);
 }
